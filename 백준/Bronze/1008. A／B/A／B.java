042c7b2476a8f8bec4060e4ff32250650dc98694
dandylsj <53831057+dandylsj@users.noreply.github.com>
2025-11-10 22:19:49 +0900

    [Bronze V] Title: A/B, Time: 180 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        
+        double A = sc.nextInt();
+        double B = sc.nextInt();
+        
+        double s = A/B;
+        System.out.println(s);
+    }
+}
